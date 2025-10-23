7ceb323f301843c18e4596e52d2b7c52a051dd12
codebygrace <gracekayode@hotmail.com>
2025-10-23 07:55:44 +0100

    feat(user): add MyUserPrincipal for authentication
    This holds details of the user currently logged in and is used by the authentication provider for SecurityContext

---
+++ backend/src/main/java/com/technicalchallenge/security/MyUserPrincipal.java
@@ -0,0 +1,54 @@
+package com.technicalchallenge.security;
+
+import com.technicalchallenge.model.ApplicationUser;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class MyUserPrincipal implements UserDetails {
+
+    private ApplicationUser applicationUser;
+
+    public MyUserPrincipal(ApplicationUser applicationUser) {
+        this.applicationUser = applicationUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return applicationUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return applicationUser.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return applicationUser.isActive();
+    }
+}
