a3b2ecc3eea594551a8252e1a76a876b6d23af94
codebygrace <gracekayode@hotmail.com>
2025-10-23 08:00:35 +0100

    feat(user): add MyUserDetailsService for authentication
    This service method is used to check if thee loginId provided by the user matches an ApplicationUser in the database and throws an exception if no matching user is found

---
+++ backend/src/main/java/com/technicalchallenge/security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.technicalchallenge.security;
+
+import com.technicalchallenge.repository.ApplicationUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    private final ApplicationUserRepository applicationUserRepository;
+
+    public MyUserDetailsService(ApplicationUserRepository applicationUserRepository) {
+        this.applicationUserRepository = applicationUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserRepository.findByLoginId(username)
+                .map(applicationUser -> new MyUserPrincipal(applicationUser))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
+
