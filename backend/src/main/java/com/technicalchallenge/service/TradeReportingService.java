15bda434065ef0126b5be817127ba8f8a2f54c12
codebygrace <gracekayode@hotmail.com>
2025-11-01 17:30:02 +0000

    feat: add service methods for trade summary statistics

--- backend/src/main/java/com/technicalchallenge/service/TradeReportingService.java
+++ backend/src/main/java/com/technicalchallenge/service/TradeReportingService.java
@@ -1,6 +1,6 @@
 package com.technicalchallenge.service;
 
-import com.technicalchallenge.model.Trade;
+import com.technicalchallenge.model.*;
 import com.technicalchallenge.repository.TradeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +8,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -25,14 +28,39 @@ public class TradeReportingService {
 
     // Find all trades for a logged in trader
     public List<Trade> getTradesByTrader(UserDetails userDetails) {
-        logger.debug("Retrieving trades for: {}", userDetails.getUsername());
+        logger.info("Retrieving trades for: {}", userDetails.getUsername());
         return tradeRepository.findByTrader(userDetails.getUsername());
     }
 
     // For Book-level trade aggregation
     public List<Trade> getTradesByBookId(Long bookId) {
-        logger.debug("Retrieving trades for book with ID: {}", bookId);
+        logger.info("Retrieving trades for book with ID: {}", bookId);
         return tradeRepository.findByBookId(bookId);
+    }
+
+    // Total number of trades by status
+    public Map<String, Long> totalTradesByStatus(UserDetails userDetails) {
+        logger.info("Counting trades by trade status for: {}", userDetails.getUsername());
+        List<Trade> userTrades = tradeRepository.findByTrader(userDetails.getUsername());
+        return userTrades.stream()
+                .collect(Collectors.groupingBy(trade -> trade.getTradeStatus().getTradeStatus(), Collectors.counting()));
+    }
+
+    // Total notional amounts by currency
+    public Map<String, BigDecimal> totalNotionalAmountsByCurrency(UserDetails userDetails) {
+        logger.info("Retrieving total notional amounts by currency for: {} ", userDetails.getUsername());
+        List<Trade> userTrades = tradeRepository.findByTrader(userDetails.getUsername());
+        return userTrades.stream().flatMap( trade -> trade.getTradeLegs().stream())
+                .collect(Collectors.groupingBy(leg -> leg.getCurrency().getCurrency(),
+                        Collectors.reducing(BigDecimal.ZERO,TradeLeg::getNotional, BigDecimal::add)));
+    }
+
+    // Breakdown count of trades by trade type and counterparty
+    public Map<String, Map<String, Long>> totalTradesByTradeTypeAndCounterparty(UserDetails userDetails) {
+        List<Trade> userTrades = tradeRepository.findByTrader(userDetails.getUsername());
+        return userTrades.stream()
+                .collect(Collectors.groupingBy(trade -> trade.getTradeType().getTradeType(),
+                        Collectors.groupingBy(trade -> trade.getCounterparty().getName(), Collectors.counting())));
     }
 
 }
