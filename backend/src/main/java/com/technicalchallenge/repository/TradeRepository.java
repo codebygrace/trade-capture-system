a33e75c2a584d8e0306958939f4b475d5e11860e
codebygrace <gracekayode@hotmail.com>
2025-11-03 17:24:15 +0000

    refactor(reporting): update repository and service methods to ensure only active trades objects are counted

--- backend/src/main/java/com/technicalchallenge/repository/TradeRepository.java
+++ backend/src/main/java/com/technicalchallenge/repository/TradeRepository.java
@@ -49,14 +49,14 @@ public interface TradeRepository extends JpaRepository<Trade, Long>, JpaSpecific
 
 
     // Method for finding trades for a given loginId
-    @Query("SELECT t FROM Trade t WHERE t.traderUser.loginId = :loginId")
-    List<Trade> findByTrader(@Param("loginId") String loginId);
+    @Query("SELECT t FROM Trade t WHERE t.traderUser.loginId = :loginId AND t.active = true")
+    List<Trade> findByTraderAndActiveTrue(@Param("loginId") String loginId);
 
     // Method for finding trades for a book with the given bookId
-    List<Trade> findByBookId(@Param("bookId") Long bookId);
+    List<Trade> findByBookIdAndActiveTrue(@Param("bookId") Long bookId);
 
-    // Method for counting trades for a given loginId and tradeDate
-    @Query("SELECT COUNT(t) FROM Trade t WHERE t.traderUser.loginId = :loginId AND t.tradeDate = :tradeDate")
+    // Method for counting active trades for a given loginId and tradeDate
+    @Query("SELECT COUNT(t) FROM Trade t WHERE t.traderUser.loginId = :loginId AND t.tradeDate = :tradeDate AND t.active = true")
     long countTradeByTraderAndTradeDate(@Param("loginId") String loginId, @Param("tradeDate") LocalDate tradeDate);
 
 }
--- backend/src/main/java/com/technicalchallenge/service/TradeReportingService.java
+++ backend/src/main/java/com/technicalchallenge/service/TradeReportingService.java
@@ -25,22 +25,22 @@ public class TradeReportingService {
         this.tradeRepository = tradeRepository;
     }
 
-    // Find all trades for a logged in trader
+    // Find all active trades for a logged in trader
     public List<Trade> getTradesByTrader(UserDetails userDetails) {
         logger.info("Retrieving trades for: {}", userDetails.getUsername());
-        return tradeRepository.findByTrader(userDetails.getUsername());
+        return tradeRepository.findByTraderAndActiveTrue(userDetails.getUsername());
     }
 
-    // For Book-level trade aggregation
+    // For Book-level trade aggregation of active trades
     public List<Trade> getTradesByBookId(Long bookId) {
         logger.info("Retrieving trades for book with ID: {}", bookId);
-        return tradeRepository.findByBookId(bookId);
+        return tradeRepository.findByBookIdAndActiveTrue(bookId);
     }
 
-    // Total number of trades by status
+    // Total number of active trades by status
     public Map<String, Long> totalTradesByStatus(UserDetails userDetails) {
         logger.info("Counting trades by trade status for: {}", userDetails.getUsername());
-        List<Trade> userTrades = tradeRepository.findByTrader(userDetails.getUsername());
+        List<Trade> userTrades = tradeRepository.findByTraderAndActiveTrue(userDetails.getUsername());
         return userTrades.stream()
                 .collect(Collectors.groupingBy(trade -> trade.getTradeStatus().getTradeStatus(), Collectors.counting()));
     }
@@ -48,7 +48,7 @@ public class TradeReportingService {
     // Total notional amounts by currency
     public Map<String, BigDecimal> totalNotionalAmountsByCurrency(UserDetails userDetails) {
         logger.info("Retrieving total notional amounts by currency for: {} ", userDetails.getUsername());
-        List<Trade> userTrades = tradeRepository.findByTrader(userDetails.getUsername());
+        List<Trade> userTrades = tradeRepository.findByTraderAndActiveTrue(userDetails.getUsername());
         return userTrades.stream().flatMap( trade -> trade.getTradeLegs().stream())
                 .collect(Collectors.groupingBy(leg -> leg.getCurrency().getCurrency(),
                         Collectors.reducing(BigDecimal.ZERO,TradeLeg::getNotional, BigDecimal::add)));
@@ -56,7 +56,7 @@ public class TradeReportingService {
 
     // Breakdown count of trades by trade type and counterparty
     public Map<String, Map<String, Long>> totalTradesByTradeTypeAndCounterparty(UserDetails userDetails) {
-        List<Trade> userTrades = tradeRepository.findByTrader(userDetails.getUsername());
+        List<Trade> userTrades = tradeRepository.findByTraderAndActiveTrue(userDetails.getUsername());
         return userTrades.stream()
                 .collect(Collectors.groupingBy(trade -> trade.getTradeType().getTradeType(),
                         Collectors.groupingBy(trade -> trade.getCounterparty().getName(), Collectors.counting())));
