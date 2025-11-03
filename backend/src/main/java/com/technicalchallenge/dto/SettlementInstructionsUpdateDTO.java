4a19fc95d14e061b82decc0fb8c2ffc3ff162141
codebygrace <gracekayode@hotmail.com>
2025-11-03 11:43:29 +0000

    refactor: update SettlementInstructionsUpdateDTO fields
    This simplifies the DTO so that it only has fields needed for updating settlement instructions

--- backend/src/main/java/com/technicalchallenge/dto/SettlementInstructionsUpdateDTO.java
+++ backend/src/main/java/com/technicalchallenge/dto/SettlementInstructionsUpdateDTO.java
@@ -3,17 +3,12 @@ package com.technicalchallenge.dto;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class SettlementInstructionsUpdateDTO {
 
-    private Long id;
     private Long entityId;
 
     @Size(min = 10, max = 500, message = "Settlement instructions must be between 10 and 500 character long")
     private String fieldValue;
 
-    private int version;
-    private LocalDateTime createdDate;
 }
